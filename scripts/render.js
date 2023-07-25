5aae028626dc18f681d72b22b520f48557f28fe5
Felicia DiPietro <95667697+feliciadipi@users.noreply.github.com>
2023-07-25 13:21:50 +0200

    insert images, change to responsive formatting

--- scripts/render.js
+++ scripts/render.js
@@ -2,9 +2,13 @@ function render(lang) {
   let title = document.getElementById('title');
   let paragraph_a = document.getElementById('a');
   let paragraph_b = document.getElementById('b');
+  let paragraph_c = document.getElementById('c');
+  let paragraph_d = document.getElementById('d');
   if (title) title.innerText = text.title[lang];
   if (paragraph_a) paragraph_a.innerText = text.a[lang];
   if (paragraph_b) paragraph_b.innerText = text.b[lang];
+  if (paragraph_c) paragraph_c.innerText = text.c[lang];
+  if (paragraph_d) paragraph_d.innerText = text.d[lang];
 }
 
 let lang = window.localStorage['lang'] ? window.localStorage['lang'] : 'it';
